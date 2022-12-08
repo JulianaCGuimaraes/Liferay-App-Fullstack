445742a4286a104428c1236586ebfc678ba54f3a
Juliana Guimarães <jujucg15@gmail.com>
2022-12-08 03:55:12 -0300

    ✨ Criação da tela de login

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { SafeAreaView, StyleSheet } from 'react-native';
+import Login from './screens/Login';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.root}>
+      <Login />
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
+  root: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor:'#0d6efd'
+  }
 });
---
+++ screens/Login/Login.js
@@ -0,0 +1,85 @@
+import React, {useState} from "react";
+import { 
+    View, 
+    Image,
+    Text, 
+    StyleSheet,
+    ScrollView
+} from 'react-native';
+import CustomInput from '../../components/CustomInput';
+import CustomButton from '../../components/CustomButton/CustomButton';
+
+const Login = () => { 
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const onSignInPressed = () => {
+        console.warn('Submit');
+    }
+
+    const onForgotPasswordPressed = () => {
+        console.warn('Esqueceu a sua senha?');
+    }
+
+    return(
+        <ScrollView showsVerticalScrollIndicator={false}>
+            <View style={styles.containerLogo}>
+                    <Image style={styles.logo} source={require('../../assets/images/liferay-logo.png')}/>
+                    <Text style={styles.titulo}>Doações</Text>
+                
+                <CustomInput 
+                    placeholder = 'Email' 
+                    value= {email} 
+                    setValue={setEmail} 
+                    />
+                <CustomInput 
+                    placeholder = 'Password' 
+                    value= {password} 
+                    setValue={setPassword} 
+                    secureTextEntry
+                    />  
+
+                <CustomButton 
+                    text='Esqueceu a sua senha?' 
+                    onPress= {onForgotPasswordPressed} 
+                    type= 'TERTIARY'
+                    fgColor= '#fff'
+                />  
+
+                <CustomButton 
+                    ftWeight= 'bold'
+                    text='Submit' 
+                    onPress= {onSignInPressed} 
+                    bgColor= '#fff'
+                /> 
+
+            </View>
+        </ScrollView>
+    );
+};
+
+const styles = StyleSheet.create({
+    containerLogo: {
+        flex: 1,
+        marginTop: '40%',
+        alignItems: 'center',
+        backgroundColor: '#0d6efd',
+        padding: 10
+    },
+    logo: {
+        width: '70%',
+        height: 70,
+        maxWidth: 300,
+        maxheight: 200,
+    },
+    titulo: {
+        color: '#fff',
+        fontSize: '25px',
+        fontWeight: 500,
+        paddingHorizontal: '50px' ,
+        marginBottom: '50px'
+    }
+});
+
+export default Login;
