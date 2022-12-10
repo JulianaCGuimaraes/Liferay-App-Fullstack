7fb2ca943763d3d0864850b758d8c4dccb0621a8
Juliana Guimarães <89886733+JulianaCGuimaraes@users.noreply.github.com>
2022-12-10 19:48:46 -0300

    Update PerguntasFrequentes.js

--- screens/PerguntasFrequentesScreen/PerguntasFrequentes.js
+++ screens/PerguntasFrequentesScreen/PerguntasFrequentes.js
@@ -6,10 +6,10 @@ import Perguntas from '../../components/Ajuda/Perguntas'
 
 const PerguntasFrequentes = () => {
     return (
-    <ScrollView style={styles.homeScreen}>
+    <ScrollView style={styles.perguntasFrequentesScreen}>
         <View>
             <View>
-                <Text style={styles.titleHome}>PERGUNTAS USUAIS</Text>
+                <Text style={styles.titlePerguntas}>PERGUNTAS USUAIS</Text>
             </View>
 
             <Perguntas />
@@ -20,40 +20,15 @@ const PerguntasFrequentes = () => {
 
 
  const styles = StyleSheet.create({
-  homeScreen: {
+  perguntasFrequentesScreen: {
     backgroundColor: 'white',
     paddingTop: 50,
     paddingHorizontal: 20
   },
-  titleHome: {
+  titlePerguntas: {
     fontSize: 26,
     fontWeight: 'bold',
     color: '#0B63CE'
-  },
-  avatarContainer: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingVertical: 10,
-  },
-  avatar: {
-    width: 185,
-    height: 185,
-    borderRadius: 95,
-    marginBottom: 25
-  }, 
-  nameUser: {
-    fontSize: 26,
-    fontWeight: 'bold',
-    color: '#0B63CE'
-  },
-  officeUser: {
-    fontSize: 20,
-    color: '#0B63CE'
-  },
-  evp: {
-    fontSize: 26,
-    color: '#0B63CE',
-    paddingTop: 20
   },
 });
 
